f5f3eaa4ef900bf55c94faeb8833924c4c9a6d92
Avishkar Deogharia <avishkardeogharia2004@gmail.com>
2025-09-11 05:25:43 +0530

    Deploy

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "http://localhost:5173" || "https://thoda-sukoon-client.vercel.app/",
   credentials: true
 }));
 app.use(express.json()); // parse JSON bodies
